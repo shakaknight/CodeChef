0acdcb883625f30bffb74659d1c00049ae6b37c8
shakaknight <59121773+shakaknight@users.noreply.github.com>
2020-09-30 01:39:41 +0530

    Create Tree2.cpp

---
+++ Tree2/Tree2.cpp
@@ -0,0 +1,25 @@
+
+int main() {
+int i,t,n;
+cin>>t;
+while(t--){
+	cin>>n;
+	int arr[n];
+		for(i=0;i<n;i++)
+	    	{
+	        	cin>>arr[i];
+	    	}
+	  	sort(arr,arr+n);
+	    	int temp=-1;
+	     	int count=0;
+	    	for(i=0;i<n;i++)
+	   	{
+	        	if(arr[i]>0 && arr[i]>temp){
+	            		temp = arr[i];
+	            		count++;
+	        	}
+	    	}
+	    	cout<<count<<"\n";
+	}
+return 0;
+}
